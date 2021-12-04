c4f70eb09056a4c45b1000300a7ebed799376f1b
schumi178 <babelm@gmail.com>
2021-12-04 23:04:02 +0100

    Działający algorytm wykrywania największej grupy pikseli o danym odcieniu

--- analysis/src/main/java/schumi178/javaprojects/graphics/zad1/analysis/PercentHueDetection.java
+++ analysis/src/main/java/schumi178/javaprojects/graphics/zad1/analysis/PercentHueDetection.java
@@ -2,6 +2,13 @@ package schumi178.javaprojects.graphics.zad1.analysis;
 
 import schumi178.javaprojects.graphics.zad1.analysis.util.ColorHSV;
 import schumi178.javaprojects.graphics.zad1.analysis.util.ColorRange;
+import schumi178.javaprojects.graphics.zad1.analysis.util.Point;
+import schumi178.javaprojects.graphics.zad1.analysis.util.SimpleColorHSV;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class PercentHueDetection {
 
@@ -15,18 +22,30 @@ public class PercentHueDetection {
         this.range = range;
     }
 
-    public double getPercentage(ColorHSV[][] image) {
-        final int imageSize = image.length * image[0].length;
-        int count = 0;
-        for (ColorHSV[] colorHSVS : image) {
-            for (ColorHSV currentColor : colorHSVS) {
+    private ColorHSV[][] getConditionMap(ColorHSV[][] image) {
+        ColorHSV[][] result = new SimpleColorHSV[image.length][image[0].length];
+        for(int i = 0; i < result.length; i++) {
+            for(int j = 0; j < result[i].length; j++) {
+                result[i][j] = new SimpleColorHSV(0, 0, 1);
+            }
+        }
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                ColorHSV currentColor = image[i][j];
                 if (range.isInRange(currentColor.getHue())) {
                     if (currentColor.getSaturation() > 50.0 / 255 && currentColor.getValue() > 3.0 / 255) {
-                        count++;
+                        result[i][j] = new SimpleColorHSV(0, 0, 0);
                     }
                 }
             }
         }
-        return (double)count / imageSize * 100;
+        return result;
+    }
+
+    public double getPercentage(ColorHSV[][] image) {
+        ColorHSV[][] conditionMap = getConditionMap(image);
+        TwoPass twoPass = new TwoPass(conditionMap);
+        double size = image.length * image[0].length;
+        return Collections.max(twoPass.getRegionSizes()) / size * 100;
     }
 }
